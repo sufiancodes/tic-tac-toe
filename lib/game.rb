561bcf68c5b1657dc4e80847f47d5db0006f2b32
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-02-20 21:25:52 +0500

    Game not responding to win bug fixed
    previously game was not responding to the win due to the error in player_two_turn method game now work as expected

--- lib/game.rb
+++ lib/game.rb
@@ -14,21 +14,23 @@ class Game < Board
   end
   def player_two_turn
     puts "#{@player_two} turn"
-    player_one_input = gets.chomp.to_i
-    index = @@array.find_index(player_one_input)
+    player_two_input = gets.chomp.to_i
+    index = @@array.find_index(player_two_input)
       @@array[index] = "O"
     render_view
   end
   def check_wining_conditions
     if check_columns || check_diagonal || check_rows
+      puts "You Win"
+      return true
     end
   end
   def play_game
     loop  do
       player_one_turn
-      break if check_wining_conditions
+      break if check_wining_conditions == true
       player_two_turn
-      break if check_wining_conditions
+      break if check_wining_conditions == true
     end
   end
 end
