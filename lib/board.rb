ad1ce4ee939b793d0cbac80470ae760f4630f6c2
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-01-23 20:36:25 +0500

    test: add test cases for the check columns method

--- lib/board.rb
+++ lib/board.rb
@@ -42,3 +42,6 @@ class Board < Player
     puts "#{@array[6]} | #{@array[7]} | #{@array[8]}"
   end
 end
+result = Board.new
+view = result.render_view
+puts view
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,21 +5,47 @@ require_relative "../lib/board"
 
 describe Board do
   let(:board) { Board.new }
+  # test cases for check_diagonal method
+
   context "Check if there is wining condition in diagonal " do
-    it "return true when all are same in primary diagonal" do
+    it "return's true when all are same in primary diagonal" do
       board.instance_variable_set(:@array, ["x", 2, 3, 4, "x", 6, 7, 8, "x"])
       result = board.check_diagonal
       expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
     end
-    it "returns true when all are same in secondary diagonal" do
+    it "return's true when all are same in secondary diagonal" do
       board.instance_variable_set(:@array, [1, 2, "O", 4, "O", 6, "O", 8, 9])
       result = board.check_diagonal
       expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
     end
-    it "return false when not same in  diagonal" do
+    it "return's false when not same in  diagonal" do
       board.instance_variable_set(:@array, ["x", 2, 3, 4, "O", 6, 7, 8, "x"])
       result = board.check_diagonal
       expect(result).to be false # rubocop:disable Style/MethodCallWithArgsParentheses
+    end
+
+    # test cases for check_columns method
+    context "Check if there is winning condition in columns" do
+      it "return's true when same in 1st column" do
+        board.instance_variable_set(:@array, ["x", 2, 3, "x", 5, 6, "x", 8, 9])
+        result = board.check_columns
+        expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
+      end
+      it "return's true when same in 2nd column" do
+        board.instance_variable_set(:@array, [1, "O", 3, 4, "O", 6, 7, "O", 9])
+        result = board.check_columns
+        expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
+      end
+      it "return's true when same in 3rd column" do
+        board.instance_variable_set(:@array, [1, 2, "x", 4, 5, "x", 7, 8, "x"])
+        result = board.check_columns
+        expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
+      end
+      it "return's false when not same in column" do
+        board.instance_variable_set(:@array, ["O", 2, 3, "X", 5, 6, 7, 8, "O"])
+        result = board.check_columns
+        expect(result).to be false # rubocop:disable Style/MethodCallWithArgsParentheses
+      end
     end
   end
 end
