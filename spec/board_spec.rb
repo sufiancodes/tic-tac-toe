4fab7b266cc4960519fa093636083d3aafefa106
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-01-23 19:58:54 +0500

    test: wrap all the diagonal related test in appropriate context

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,19 +5,21 @@ require_relative "../lib/board"
 
 describe Board do
   let(:board) { Board.new }
-  it "return true when all are same in primary diagonal" do
-    board.instance_variable_set(:@array, ["x", 2, 3, 4, "x", 6, 7, 8, "x"])
-    result = board.check_diagonal
-    expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
-  end
-  it "returns true when all are same in secondary diagonal" do
-    board.instance_variable_set(:@array, [1, 2, "O", 4, "O", 6, "O", 8, 9])
-    result = board.check_diagonal
-    expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
-  end
-  it "return false when not same in  diagonal" do
-    board.instance_variable_set(:@array, ["x", 2, 3, 4, "O", 6, 7, 8, "x"])
-    result = board.check_diagonal
-    expect(result).to be false # rubocop:disable Style/MethodCallWithArgsParentheses
+  context "Check if there is wining condition in diagonal " do
+    it "return true when all are same in primary diagonal" do
+      board.instance_variable_set(:@array, ["x", 2, 3, 4, "x", 6, 7, 8, "x"])
+      result = board.check_diagonal
+      expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
+    end
+    it "returns true when all are same in secondary diagonal" do
+      board.instance_variable_set(:@array, [1, 2, "O", 4, "O", 6, "O", 8, 9])
+      result = board.check_diagonal
+      expect(result).to be true # rubocop:disable Style/MethodCallWithArgsParentheses
+    end
+    it "return false when not same in  diagonal" do
+      board.instance_variable_set(:@array, ["x", 2, 3, 4, "O", 6, 7, 8, "x"])
+      result = board.check_diagonal
+      expect(result).to be false # rubocop:disable Style/MethodCallWithArgsParentheses
+    end
   end
 end
