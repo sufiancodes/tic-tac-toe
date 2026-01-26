9376556118e01c48ee04a56f9acce0e7bc79d964
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-01-26 20:06:53 +0500

    test: add remaining test and test for Victory Statement

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,13 +5,20 @@ require_relative "../lib/game"
 describe Game do
   let(:game) { Game.new }
   context "Check winning condition" do
-    it "return's true when there is victory in column" do
-      game.array = ["x", 2, 3, "x", 5, 6, "x", 8, 9]
-      expect(game.check_wining_conditions).to(eq(true))
+    it "it print to the screen You Win" do
+      game.array = ["O", "O", "O", 4, 5, 6, 7, 8, 9]
       expect { game.check_wining_conditions }.to(output("You Win\n").to_stdout)
     end
-    it "return's true when there is victory in row" do
+    it "return's true when there is match in column" do
+      game.array = ["x", 2, 3, "x", 5, 6, "x", 8, 9]
+      expect(game.check_wining_conditions).to(eq(true))
+    end
+    it "return's true when there is match in row" do
       game.array = ["x", "x", "x", 4, 5, 6, 7, 8, 9]
+      expect(game.check_wining_conditions).to(eq(true))
+    end
+    it "return's true when there is match in diagonal" do
+      game.array = ["O", 2, 3, 4, "O", 6, 7, 8, "O"]
       expect(game.check_wining_conditions).to(eq(true))
     end
   end
